10971b0ce2ac22b4023bbeabbb127296e0e4740d
Yuki Naka <yukinaka2168@au.com>
2024-06-19 21:42:59 +0900

    「MainActivity」内の「fragmentTransaction」関連の不要なコードを削除。前の画面が残る現象を無事回避。

--- app/src/main/java/com/example/hito_memo/MainActivity.kt
+++ app/src/main/java/com/example/hito_memo/MainActivity.kt
@@ -4,16 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val fragment = MainFragment()
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-
-        fragmentTransaction.add(R.id.activity_main, fragment)
-        fragmentTransaction.commit()
     }
-
 }
