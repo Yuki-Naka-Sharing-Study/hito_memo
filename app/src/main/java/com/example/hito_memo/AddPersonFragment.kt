9d20da5091c22f08994f7f9283ed22e0cb3370d7
Yuki Naka <yukinaka2168@au.com>
2024-06-20 17:03:39 +0900

    ScrollViewを「android:visibility="visible"」にし、 EditTextを「android:visibility="gone"」にし
    「addOnButtonCheckedListener」で
    切り替えることに成功。
    
    しかし、二回タップしないと反応しないバグ？が発生。

--- app/src/main/java/com/example/hito_memo/AddPersonFragment.kt
+++ app/src/main/java/com/example/hito_memo/AddPersonFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isGone
 import androidx.navigation.fragment.findNavController
 import com.example.hito_memo.databinding.FragmentAddPersonBinding
 
@@ -33,6 +35,26 @@ class AddPersonFragment : Fragment() {
         binding.textViewAddPerson.setOnClickListener {
             navController.navigate(R.id.action_addPersonFragment_to_mainFragment)
         }
+
+        // GPTに聞いた感じではこの書き方が合っていそう
+        binding.toggleButtonProfileAndMemo.addOnButtonCheckedListener { materialButtonToggleGroup, checkedId, isChecked ->
+            if (isChecked) {
+                when (checkedId) {
+                    R.id.toggle_button_profile -> showProfile()
+                    R.id.toggle_button_memo -> showMemo()
+                }
+            }
+        }
+    }
+
+    private fun showProfile() {
+        binding.scrollViewOfProfileAndMemo.visibility = View.VISIBLE
+        binding.editTextMemoOfPerson.visibility = View.GONE
+    }
+
+    private fun showMemo() {
+        binding.editTextMemoOfPerson.visibility = View.VISIBLE
+        binding.scrollViewOfProfileAndMemo.visibility = View.GONE
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/hito_memo/MainActivity.kt
+++ app/src/main/java/com/example/hito_memo/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.hito_memo
 
+import android.content.Context
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //画面がタッチされた時に反応するやつ
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        // InputMethodManagerを取得
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        //背景のlinearLayoutを取得
+        val constraintLayout = findViewById<ConstraintLayout>(R.id.activity_main)
+        // キーボードを閉じる
+        inputMethodManager.hideSoftInputFromWindow(
+            constraintLayout.windowToken,
+            InputMethodManager.HIDE_NOT_ALWAYS
+        )
+        return false
     }
 }
