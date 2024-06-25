d841ce1e70574c2ed184bf33a1de3e1aaaeab5a3
Yuki Naka <yukinaka2168@au.com>
2024-06-25 15:49:16 +0900

    「HitoMemoFragment」から「SettingFragment」へ画面遷移して戻る処理を追加。「MainFragment」の「created_folder」の「ListView」をデフォルトで「visibility=gone」で見えなくした。

--- app/src/main/java/com/example/hito_memo/HitoMemoFragment.kt
+++ app/src/main/java/com/example/hito_memo/HitoMemoFragment.kt
@@ -5,18 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.hito_memo.databinding.FragmentHitoMemoBinding
 
 class HitoMemoFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentHitoMemoBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hito_memo, container, false)
+    ): View {
+        _binding = FragmentHitoMemoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+
+        binding.backToSettingViewImageButton.setOnClickListener {
+            navController.navigate(R.id.action_hitoMemoFragment_to_settingFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
