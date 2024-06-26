ba375870ef9739b50c18609a25932f06a9bc3865
Yuki Naka <yukinaka2168@au.com>
2024-06-26 13:49:36 +0900

    「HitoMemoProFragment」の「backToSettingViewTextView」をタップした際も設定画面に戻ることができるように追記。

--- app/src/main/java/com/example/hito_memo/HitoMemoProFragment.kt
+++ app/src/main/java/com/example/hito_memo/HitoMemoProFragment.kt
@@ -32,7 +32,13 @@ class HitoMemoProFragment : Fragment() {
 
         val navController = findNavController()
 
+        // 以下の二つにbindingは同じ処理をしているがViewで囲ってViewをタップした時に処理を行うようにした方がいいか？
+
         binding.backToSettingViewImageButton.setOnClickListener {
+            navController.navigate(R.id.action_hitoMemoProFragment_to_settingFragment)
+        }
+
+        binding.backToSettingViewTextView.setOnClickListener {
             navController.navigate(R.id.action_hitoMemoProFragment_to_settingFragment)
         }
 
