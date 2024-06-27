f7a0f3c653612ef843633ab7c9c2fef37c7c54c0
Yuki Naka <yukinaka2168@au.com>
2024-06-27 11:21:05 +0900

    「fragment_profile_customize.xml」の大枠完成。

---
+++ app/src/main/java/com/example/hito_memo/ProfileCustomizeFragment.kt
@@ -0,0 +1,38 @@
+package com.example.hito_memo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.hito_memo.databinding.FragmentProfileCustomizeBinding
+
+class ProfileCustomizeFragment : Fragment() {
+
+    private var _binding: FragmentProfileCustomizeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileCustomizeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+
+        // binding.
+            // navController.navigate(R.id)
+        // }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
