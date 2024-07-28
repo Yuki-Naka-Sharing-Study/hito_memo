177fe8d6b0ef6b789c32ce0f6ffd34be1b364921
Yuki Naka <yukinaka2168@au.com>
2024-07-28 11:59:37 +0900

    UserRepository.ktをリファクタリング。

--- app/src/main/java/com/example/hito_memo/UserRepository.kt
+++ app/src/main/java/com/example/hito_memo/UserRepository.kt
@@ -3,9 +3,10 @@ package com.example.hito_memo
 import kotlinx.coroutines.flow.map
 
 class UserRepository(private val userDao: UserDao) {
+
     fun getUsers() = userDao.getAll()
 
-    fun getNameOfUserFolder() = getUsers().map {
+    fun getNameOfUserFolder() = userDao.getAll().map {
             it -> it.sortedBy { it.nameOfUserFolder }
     }
 }
