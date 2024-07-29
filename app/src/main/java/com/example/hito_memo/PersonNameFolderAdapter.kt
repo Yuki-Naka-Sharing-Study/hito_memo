fc2cfef63052ce10bb4d9eda3234b8ca7d6034d9
Yuki Naka <yukinaka2168@au.com>
2024-07-29 14:25:32 +0900

    PersonNameFolderAdapter.ktとperson_name_folder_list_item.xmlの作成。

---
+++ app/src/main/java/com/example/hito_memo/PersonNameFolderAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.hito_memo
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.example.hito_memo.databinding.PersonNameFolderListItemBinding
+
+class PersonNameFolderAdapter(private val context: Context, private val dataSource: List<String>) : BaseAdapter() {
+
+    private val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getCount(): Int {
+        return dataSource.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return dataSource[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val binding: PersonNameFolderListItemBinding
+        val view: View
+
+        if (convertView == null) {
+            binding = PersonNameFolderListItemBinding.inflate(inflater, parent, false)
+            view = binding.root
+            view.tag = binding
+        } else {
+            binding = convertView.tag as PersonNameFolderListItemBinding
+            view = convertView
+        }
+
+        val createdFolderItem = getItem(position) as String
+        binding.eachFolderTextView.text = createdFolderItem
+
+        return view
+    }
+}
