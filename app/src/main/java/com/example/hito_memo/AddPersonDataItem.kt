a87215c0a996319ea30447a9867ee5a118bbe79a
Yuki Naka <yukinaka2168@au.com>
2024-07-30 12:32:38 +0900

    「AddPersonDataItem」に「var profileEditTextHint: String?,」を追加。

--- app/src/main/java/com/example/hito_memo/AddPersonDataItem.kt
+++ app/src/main/java/com/example/hito_memo/AddPersonDataItem.kt
@@ -3,8 +3,9 @@ package com.example.hito_memo
 data class AddPersonDataItem(
 
     var profileTextViewString : String?,
-    var profileEditTextString: String?,
+    var profileEditTextHint: String?,
     var profileCustomEditTextString: String?,
-    var profileImageViewInt: Int?
+    var profileImageViewInt: Int?,
+    var profileEditTextString: String?
 
 )
